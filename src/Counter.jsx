de450d76ec08e0e67ff8cda244f71f0ca37ee256
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-08 12:59:08 +0600

    function based

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,10 +3,24 @@ import React, { useState } from 'react';
 function Counter() {
   const [count, setCount] = useState(0);
 
+  const increment = () => {
+    setCount(count + 1);
+  }
+
+  const decrement = () => {
+    setCount(count - 1);
+  }
+
+  const reset = () => {
+    setCount(0);
+  }
+
   return (
-    <div>
-      <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>Counter Increment</button>
+    <div className='counter-container'>
+      <p className='count-display'>{count}</p>
+        <button className='counter-button' onClick={decrement}>Decrement</button>
+        <button className='counter-button' onClick={reset}>Reset</button>
+        <button className='counter-button' onClick={increment}>Increment</button>
     </div>
   );
 }
