fb1a3b82ebf8c533a3aa431400e77a44ea7d1ff1
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-08 11:05:24 +0600

    increment Age

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -3,15 +3,23 @@ import React, {useState} from 'react';
 function MyComponent() {
  
     const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(25);
+
 
     const updateName = () => {
         setName("Tanjir Hasan");
         console.log("Name has been set to Tanjir Hasan");
     }
+    const incrementAge = () => {
+        setAge(age + 1);
+        console.log("Age has been incremented");
+    }
 
     return(<div>
         <p>Name: {name}</p>
         <button onClick={updateName}>Set Name</button>
+        <p>Age: {age}</p>
+        <button onClick={incrementAge}>Increment Age</button>
     </div>)
 }
 
